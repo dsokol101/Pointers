884754306bbe472e15898a04481283a6d8eb95d0
dsokol101 <dsokol@brooklyn.cuny.edu>
2025-03-04 10:44:12 -0500

    dynamic memory

--- dynamic_mem.cpp
+++ dynamic_mem.cpp
@@ -1,19 +1,47 @@
 #include <iostream>
 using namespace std;
-/* 2/27/2025 NEXT TIME
+/* 3/4/2025 
 
 What is dynamic memory?
 memory allocated during runtime (on the heap)
+opposite of automatic memory (on the stack)
+why use dynamic memory?
+
+- when you don't know the size of the array until runtime
+- when you need more memory than is allowed on the stack
+
+- when you need to allocate memory for an object that will be returned from a function
+
 */
+void practice() {
+    int num{0};
+    cout <<"How many elts are in your array? ";
+    cin >> num;
+    int* arr = new int[num]{5, 4}; // allocate memory for an array of ints
+    // the empty braces initialize the memory to 0
+    for (int i{0}; i < num; ++i) {
+        cout << arr[i] << " ";
+    }
+    delete[] arr; // deallocate memory
+    arr = nullptr; // set arr to nullptr to avoid dangling pointer
+}
 int main() {
-    int i = 10;
-    // int* means pointer to an integer
-    // & is an operator that means "THE ADDRESS OF"
-    int* iptr = &i;   
-    cout << "Value of i: " << i << endl;
-    cout << "Address of i: " << &i << endl;
+    // int i = new int; // error: new returns a pointer to the allocated memory
+    
+    // new allocates memory on the heap and returns a pointer to that memory
+    int* iptr = new int{}; // iptr is a pointer to an integer   
+    // the empty braces initializes the memory to 0
+    // if you don't initialize the memory, it will contain junk
+
+    *iptr = 10; // dereference iptr and assign 10 to the memory it points to
     cout << "Value of iptr: " << iptr << endl;
     cout << "Value of variable that iptr points to: " << *iptr << endl;
-   
+    delete iptr; // deallocate the memory
+    iptr = nullptr; // set iptr to nullptr to avoid dangling pointer
+    cout << "Value of iptr after delete: " << iptr << endl;
+    if (iptr) {
+        cout << "value of *iptr: " << *iptr << endl;
+    }
+    practice();
     return 0;
 }
