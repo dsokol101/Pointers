fe9e60fb0d33f969072a589db35d912dfce375b6
dsokol101 <dsokol@brooklyn.cuny.edu>
2025-09-30 11:03:09 -0400

    9/29 in class changes

--- pointer_intro.cpp
+++ pointer_intro.cpp
@@ -10,7 +10,16 @@ int main() {
     cout << "Value of i: " << i << endl;
     cout << "Address of i: " << &i << endl;
     cout << "Value of iptr: " << iptr << endl;
+    // iptr=25; illegal to assign an int to a pointer variable
     cout << "Value of variable that iptr points to: " << *iptr << endl;
+    *iptr = 15; // change the value of i through the pointer
+    cout << "Value of i: " << i << endl;
+    // pointer variable can change like any variable
+    int j = 30;
+    iptr = &j; // now iptr points to j
+    cout << "Value of variable that iptr points to: " << *iptr <<
+            endl;
+
     double d = 3.14;
     double* dptr = &d;
     d=d+1.0;
@@ -18,16 +27,13 @@ int main() {
     *dptr += 1.0;
     cout << "Value of d: " << d << endl;
 
-    int num = 100;
-    iptr = &num;
     // uninitialized pointer -- also called a dangling pointer
     // DANGEROUS -- can cause segmentation faults
     int* iptr2;
     cout << "contents of iptr2: " << *iptr2 << endl;
 
-    // not to be confused with reference parameters
+    // not to be confused with reference variables
     // int& means reference to an integer
-    // & is an operator that means "REFERENCE TO"
     // use reference parameters with functions when you want the function to
     // modify the actual value of the variable
     int& iref = i;
--- pointers_functions.cpp
+++ pointers_functions.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-/** 2/27
+/**
  * Pointers as function parameters
    when you pass the address of a variable into a function, if the function
    modifies the value at that address, the change will be reflected in the 
@@ -8,15 +8,26 @@
 
 
 using namespace std;
+
+void swap(int* p1, int* p2) {
+    int temp = *p1;
+    *p1=*p2;
+    *p2=temp;
+}
+
 // this function modifies the value that iptr points to
 // it is in essence the same as passing by reference
 void increment(int* iptr) {
-    (*iptr)++;
+    // iptr++; this would increment the pointer, not the value it points to
+    (*iptr)++; // increment the value that iptr points to
 }
 int main() {
-    int num=10;
-    cout << "Value of num: " << num << endl;
-    increment(&num);
-    cout << "Value of num after increment: " << num << endl;
+    int num1=20;
+    cout << "Value of num1: " << num1 << endl;
+    increment(&num1);
+    cout << "Value of num1 after increment: " << num1 << endl;
+    int num2=5;
+    swap(&num1, &num2);
+    cout << "After swap, num1: " << num1 << ", num2: " << num2 << endl;
     return 0;
 }
